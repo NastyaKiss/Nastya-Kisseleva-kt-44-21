6d0cfdfefcd0bc3718d093f7b7a0da43d9b9a29d
Petrova Valentina <valya@chuvsu.ru>
2024-10-03 16:18:34 +0300

    03.10.2024

---
+++ Proect/DataBase/StudentDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace KiselevaAnastasiaKt_44_21.DataBase
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<
+            StudentDbContext> options) : base(options) { 
+        }
+    }
+}
---
+++ Proect/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace KiselevaAnastasiaKt_44_21.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public int GroupId { get; set; }
+
+        public Group Group { get; set; }
+
+    }
+}
